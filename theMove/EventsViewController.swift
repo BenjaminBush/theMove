27581f7a5d3969e6fdd72dfc56324d36362df368
Rachel Partridge <rpartridge@wustl.edu>
2017-03-22 17:48:19 -0500

    tabs

--- theMove/EventsViewController.swift
+++ theMove/EventsViewController.swift
@@ -8,10 +8,53 @@
 
 import UIKit
 
-class EventsViewController: UIViewController {
+struct moveData {
+    var eventName: String
+    var friendsGoing: Int
+    var peopleGoing: Int
+}
 
+class EventsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    var events:[String] = ["dog", "cat", "eat ice cream", "go to T's"]
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell =  UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        cell.textLabel!.text = events[indexPath.row]
+        cell.detailTextLabel?.text = "10 friends, 30 people"
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return events.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let details = EventDetailViewController(nibName: "EventDetailViewController", bundle: nil)
+        navigationController?.pushViewController(details, animated: true)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            //self.cacheImages()
+            
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+        self.title = "Moves"
 
         // Do any additional setup after loading the view.
     }
