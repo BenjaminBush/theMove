3f5965f64943cecf8017f1f36181c2ece82b4387
BenjaminBush <ben.bush4@gmail.com>
2017-03-27 22:03:40 -0500

    cleanup + addEvent

---
+++ backend/addEvent.php
@@ -0,0 +1,70 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: benbush
+     * Date: 2/18/17
+     * Time: 1:33 PM
+     */
+    // Use htmlentities to parse bad strings
+    $host = htmlentities($_REQUEST["host"]);
+    $name = htmlentities($_REQUEST["name"]);
+    $date = htmlentities($_REQUEST["date"]);
+    $addr = htmlentities($_REQUEST["address"]);
+    $numGuests = 0;
+    $category = htmlentities($_REQUEST["category"]);
+    $active = htmlentities($_REQUEST["active"]);
+
+ 
+    // Make sure all variables have value
+    if (empty($host) || empty($name) || empty($date) || empty($addr) || empty($category) || !isset($active)) {
+        $returnArray["status"] = "400";
+        $returnArray["message"] = "Missing required information";
+        echo json_encode($returnArray);
+        return;
+    }
+
+    if ($category != "Cultural" || $category != "Party"|| $category != "Chill" || $category != "Community Oriented" || $category != "Free") {
+        $returnArray["status"] = "400";
+        $returnArray["message"] = "Invalid event category";
+        echo json_encode($returnArray);
+        return;
+    }
+
+    require 'database.php';
+
+    $stmt0 = $mysqli->prepare("SELECT user_id FROM users WHERE username=?");
+    if (!$stmt0) {
+	printf("Query Prep Failed: %s\n", $mysqli->error);
+	exit;
+    }
+
+    $stmt0->bind_param('s', $host);
+    $stmt0->execute();
+
+    $stmt0->bind_result($host_id);
+    $stmt0->fetch();
+    $stmt0->close();
+
+    $stmt = $mysqli->prepare("INSERT INTO events(host_id, event_name, event_date, event_address, numGuests, category, active) VALUES(?, ?, ?, ?, ?, ?, ?)");
+    
+
+    if (!$stmt) {
+        printf("Query Prep Failed: %s\n", $mysqli->error);
+    }
+
+    $stmt->bind_param('isssisi', $host_id, $name, $date, $addr, $numGuests, $category, $active);
+    $stmt->execute();
+
+    if ($stmt) {
+        $returnArray["status"] = "200";
+	    $returnArray["message"] = "OK";
+	    echo json_encode($returnArray);
+	    return json_encode($returnArray);
+    } else {
+	    $returnArray["status"] = "400";
+	    $returnArray["message"] = "Failed to insert user into the database";
+	    echo json_encode($returnArray);
+	    return json_encode($returnArray);
+    } 
+    $stmt->close();
+?>
